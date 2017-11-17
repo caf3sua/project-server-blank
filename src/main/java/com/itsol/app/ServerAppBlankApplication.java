5cc55254c1ea7e037cb6a2b015b15a5193947541
Nam Nguyen Hoai <hoainamtin2@gmail.com>
2017-11-17 17:44:49 +0700

    init project

---
+++ src/main/java/com/itsol/app/ServerAppBlankApplication.java
@@ -0,0 +1,12 @@
+package com.itsol.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAppBlankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerAppBlankApplication.class, args);
+	}
+}
